6566e14e7a15c5fb49c34878fcb77f46b023af11
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2024-06-11 16:57:09 +0200

    Start a simple TCP listener
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net"
+)
+
+func handleConnection(conn net.conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err)
+	}
+
+	message := string(buf[:n])
+	fmt.Println("Received message ", message)
+}
+
+func startServer(string address) {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println("Error starting listener: ", err)
+		return
+	}
+
+	defer listener.Close()
+	fmt.Println("Starting argeos daemon on ", address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+
+}
+
+
+func main() {
+	startServer(":9999")
+}
