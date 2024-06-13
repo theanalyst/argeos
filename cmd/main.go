32624ec47d5076a36cb22556d666244e79f121c8
Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>
2024-06-13 09:46:13 +0200

    main: add simple config flag for path to config file
    Signed-off-by: Abhishek Lekshmanan <abhishek.lekshmanan@cern.ch>

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"gitlab.cern.ch/eos/argeos/internal/config"
 	"gitlab.cern.ch/eos/argeos/internal/server"
 )
 
 func main() {
+	var configpath string
+	flag.StringVar(&configpath, "c", "/etc/argeos.config.json",
+		"Path to config file [/etc/argeos.config.json]")
 
 	config := config.ConfigurefromFile("config.json")
 	server.StartServer(config.Server.Address)
